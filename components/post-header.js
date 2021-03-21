8f3d6fcab007cfa45b158ff5ee92dd548f73b535
Fabrício <fabricio10@gmail.com>
2021-03-20 21:33:23 -0300

    v3

--- components/post-header.js
+++ components/post-header.js
@@ -3,13 +3,11 @@ import Date from '../components/date'
 import CoverImage from '../components/cover-image'
 import PostTitle from '../components/post-title'
 
-export default function PostHeader({ title, coverImage, date, author }) {
+export default function PostHeader({ title, coverImage, date }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
-      </div>
+      
       <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
         <CoverImage
           title={title}
