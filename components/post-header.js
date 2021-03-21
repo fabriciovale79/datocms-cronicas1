7c6e6561bcaf8fa02581b71ec919a5547a2f1ca9
Fabrício <fabricio10@gmail.com>
2021-03-20 21:00:38 -0300

    v2.1

--- components/post-header.js
+++ components/post-header.js
@@ -17,9 +17,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
         />
       </div>
       <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
+        
         <div className="mb-6 text-lg">
           <Date dateString={date} />
         </div>
