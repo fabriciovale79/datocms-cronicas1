36972edd67c9f3998be9b1f6eec9f729e4e963a6
Fabrício <fabricio10@gmail.com>
2021-08-18 22:57:38 -0300

    v4

--- components/nav-footer.js
+++ components/nav-footer.js
@@ -4,17 +4,21 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import Container from './container'
+import { left } from '@popperjs/core';
+import { useState } from 'react';
 
 export default function NavFooter({posts}) {
+  const {left, right, home} = useState('BottomNavigation',{left:'/',right:'prefacio',home:'/'});
 
   return (
     <Container>
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
         <BottomNavigation
           showLabels
-          value="val"
+          value="value"
+
         >
-          <BottomNavigationAction label="Recents" icon={<ArrowBackIcon />} />
+          <BottomNavigationAction label="Recents" icon={<ArrowBackIcon />}  />
           <BottomNavigationAction label="Favorites" icon={<MenuBookIcon />} />
           <BottomNavigationAction label="Archive" icon={<ArrowForwardIcon />} />
         </BottomNavigation>
